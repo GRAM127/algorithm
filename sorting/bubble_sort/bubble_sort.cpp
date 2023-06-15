a7608d890888bbb3b787477ff667b2e6e74280e6
GRAM127 <dev.gram127@gmail.com>
2023-06-15 22:35:55 +0900

    Code: 버블 정렬 코드 작성  `C++` `Rust`
    Add "bubble_sort.cpp"
    Add "bubble_sort.rs"

---
+++ sorting/bubble_sort/bubble_sort.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+void bubble_sort(vector<T>& vec) {
+  size_t len = vec.size();
+  
+  // 한 번 반복할 때마다 요소 하나가 정렬됨
+  for (size_t i = len - 1; i >= 1; i--) {
+   
+    // 정렬 완료 여부를 확인하는 변수
+    bool sorted = true;
+    
+    for (size_t j = 0; j < i; j++) {
+      if (vec[j] > vec[j + 1]) {
+        // 앞 요소가 크다면 교환
+        swap(vec[j], vec[j + 1]);
+        sorted = false;
+      }
+    }
+
+    if (sorted) {
+      break;
+    }
+  }
+}
